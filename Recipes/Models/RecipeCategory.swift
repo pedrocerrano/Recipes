9a8b356ec481c941a3ea3579321088b8b3653eca
pedrocerrano <82617951+pedrocerrano@users.noreply.github.com>
2023-02-13 11:15:51 -0600

    Completes model setup

---
+++ Recipes/Models/RecipeCategory.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeCategory.swift
+//  Recipes
+//
+//  Created by iMac Pro on 2/13/23.
+//
+
+import Foundation
+
+class RecipeCategory {
+    let id: UUID
+    var title: String
+    var recipes: [Recipe]
+    
+    init(id: UUID = UUID(), title: String, recipes: [Recipe] = []) {
+        self.id = id
+        self.title = title
+        self.recipes = recipes
+    } //: MEMBERWISE INITIALIZER
+} //: CLASS
